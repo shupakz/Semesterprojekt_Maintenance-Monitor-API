1131e52d7977f8faae25765c09cfeaf97aabd5ec
shupakz <wi22b035@technikum-wien.at>
2023-06-30 21:08:27 +0200

    Initial commit

---
+++ src/main/java/at/technkium/Maintenance/Monitor/API/MaintenanceMonitorApiApplication.java
@@ -0,0 +1,13 @@
+package at.technkium.Maintenance.Monitor.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceMonitorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/technkium/Maintenance/Monitor/API/MaintenanceMonitorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technkium.Maintenance.Monitor.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
